c0683676accf80e038a0be878344c08bfa454df7
GraficoGpt <graficogpt@gmail.com>
2025-04-27 19:00:05 -0500

    Ajustes en head y varios otros items

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel"
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
+import vercel from '@astrojs/vercel'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.principedelosparamos.org',
   output: 'server',
   adapter: vercel(),
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
-});
+})
